a0a588040dcf873e1123f67f6b2cb4f991dea37e
Andrea Carmisciano <andrea.carmisciano@gmail.com>
2018-11-21 09:19:44 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='iqualitypy',
-    version='0.0.1',
+    version='0.0.2',
     description='Python iQuality API library',
     license='MIT',
     packages=['iqualitpy'],
